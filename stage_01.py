18671a3d1caa4a724188dc9c3aadfdb9df39a9d7
gourav <dtu.gourav@gmail.com>
2022-01-12 09:33:11 +0530

    first commit

---
+++ stage_01.py
@@ -0,0 +1,10 @@
+# MLOps Lectures
+# started in gitbash as terminal
+# conda create -n mlops python=3.7 -y
+
+# conda activate mlops
+
+text = "input 01"
+
+with open("artifacts01.txt", 'w') as f: 
+    f.write(text)
